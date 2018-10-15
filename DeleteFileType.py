cedeef67c33c6ef23644a5ef8953d29a6d15d06c
Eric Pang <eric.pang@live.com>
2018-10-14 20:27:08 -0400

    Change naming convention

--- DeleteFileType.py
+++ DeleteFileType.py
@@ -2,30 +2,30 @@
 import os
 
 path = "C:\\Users\\"  # Put path to folder where you want to search
-fileType = ""  # File Type
+file_type = ""  # File Type
 
-def GetFiles(str):
+def get_files(str):
     if(str == "print"):
         # Prints files about to be deleted
         for file in os.listdir(path):
-            if file.endswith(fileType):
+            if file.endswith(file_type):
                 print(file)
         return
 
     if(str == "delete"):
         # Deletes files
         for file in os.listdir(path):
-            if file.endswith(fileType):
+            if file.endswith(file_type):
                 tempPath = path + "\\" + file
                 os.remove(tempPath)
     return
 
-fileType = input("What file type do you want to remove?\n").strip()
+file_type = input("What file type do you want to remove?\n").strip()
 print("Make sure you edited this program to include the folder path!\n")
-print("Do you want to remove these files (forever) ending with "+fileType+"?\n")
-GetFiles("print")
+print("Do you want to remove these files (forever) ending with " + file_type + "?\n")
+get_files("print")
 
 if input("\n(y) Yes | (n) No\n").strip().lower() == "y":
-    GetFiles("delete")
+    get_files("delete")
 else:
     raise SystemExit
